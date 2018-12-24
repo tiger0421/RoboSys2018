f3e5f88cf6397884e78385fb2afb80dac6eb6b1e
sima <taiger0421@yahoo.co.jp>
2018-12-24 22:58:28 +0900

    first commit

---
+++ myled.c
@@ -0,0 +1,87 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
+#include <linux/random.h>
+MODULE_AUTHOR("Hiroki Shimada");
+MODULE_DESCRIPTION("Rock-Paper-Scissors");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+static dev_t dev;
+static struct cdev cdv;
+static struct class *cls = NULL;
+static volatile u32 *gpio_base = NULL;
+
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){
+    char hand, cp_hand;
+    char ex_hand[4] = {'g', 'x', 'p', '\0'};
+    if(copy_from_user(&hand,buf,sizeof(char))) return -EFAULT;
+    if(hand != '\n'){
+    	cp_hand = get_random_int();
+	cp_hand = cp_hand % 3 + 1;
+    	printk(KERN_INFO "cp_hand = %c\n",ex_hand[cp_hand-1]);
+
+//cp_hand: 1 = Rock, 2 = Scissors, 3 = Paper
+    	if((hand == 'g' && cp_hand == 2) || (hand == 'x' && cp_hand == 3) || (hand == 'p' && cp_hand == 1)){
+        	gpio_base[7] = 1<<25;
+    	}
+    	else{
+        	gpio_base[10] = 1<<25;
+    	}
+    }
+    return 1;
+}
+
+static ssize_t howTo_read(struct file* filp, char* buf, size_t count, loff_t* pos){
+  int size = 0;
+  printk(KERN_INFO "Rock  	->	g\n");
+  printk(KERN_INFO "Scissors	->	x\n");
+  printk(KERN_INFO "Paper	->	p\n");
+  return 0;
+}
+
+
+static struct file_operations led_fops = {
+        .owner = THIS_MODULE,
+        .write = led_write,
+        .read = howTo_read
+};
+
+static int __init init_mod(void){
+    int retval;
+    gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+    const u32 led = 25;
+    const u32 index = led/10;
+    const u32 shift = (led%10)*3;
+    const u32 mask = ~(0x7 << shift);
+    gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+    retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+    printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__, MAJOR(dev));
+
+    cdev_init(&cdv, &led_fops);
+    retval = cdev_add(&cdv, dev, 1);
+    if(retval < 0){
+        printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+        return retval;
+        }
+    cls = class_create(THIS_MODULE, "myled");
+    if(IS_ERR(cls)){
+        printk(KERN_ERR "class_create failed.");
+        return PTR_ERR(cls);
+    }
+    device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+    return 0;
+}
+
+static void __exit cleanup_mod(void){
+    cdev_del(&cdv);
+    device_destroy(cls, dev);
+    class_destroy(cls);
+    unregister_chrdev_region(dev, 1);
+    printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__, MAJOR(dev));
+}
+
+module_init(init_mod);
+module_exit(cleanup_mod);
---
+++ myled.mod.c
@@ -0,0 +1,51 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0x367398b6, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
+	{ 0xdb7c2b58, __VMLINUX_SYMBOL_STR(class_destroy) },
+	{ 0x5eaebe1a, __VMLINUX_SYMBOL_STR(device_destroy) },
+	{ 0x1a96fe1c, __VMLINUX_SYMBOL_STR(cdev_del) },
+	{ 0xcbbf3ca9, __VMLINUX_SYMBOL_STR(device_create) },
+	{ 0x7e20ba1d, __VMLINUX_SYMBOL_STR(__class_create) },
+	{ 0x12d86225, __VMLINUX_SYMBOL_STR(cdev_add) },
+	{ 0xede71573, __VMLINUX_SYMBOL_STR(cdev_init) },
+	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0x79c5a9f0, __VMLINUX_SYMBOL_STR(ioremap) },
+	{ 0x4e6d24c3, __VMLINUX_SYMBOL_STR(get_random_u32) },
+	{ 0xfa2a45e, __VMLINUX_SYMBOL_STR(__memzero) },
+	{ 0x28cc25db, __VMLINUX_SYMBOL_STR(arm_copy_from_user) },
+	{ 0xf7802486, __VMLINUX_SYMBOL_STR(__aeabi_uidivmod) },
+	{ 0x2e5810c6, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr1) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xb1ad28e0, __VMLINUX_SYMBOL_STR(__gnu_mcount_nc) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "AEF34F1566AB3B24A74705B");
